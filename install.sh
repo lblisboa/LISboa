4e91dbfae9f85d384bb280a12f3f27435e2beacc
lblisboa <85908555+lblisboa@users.noreply.github.com>
2021-06-18 20:33:15 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -7,7 +7,7 @@ GREEN="\033[1;32m"
 RED='\033[1;31m'
 NC='\033[0m'
 
-echo $YELLOW"[$RED!$YELLOW] Eu vou instalar as dependências essenciais pro bot funcinar você só concorda com os programas que vou instalar blz? [s/n]"
+echo $YELLOW"[$RED!$YELLOW] VOU INSTALAR ALLGUNS OK? [s/n]"
 read inp
 if [ "$inp" = "s" ]; then
     pkg update
@@ -23,10 +23,10 @@ if [ "$inp" = "s" ]; then
     pkg install imagemagick 
     npm install
     npm install pm2 -g
-    echo $GREEN"Hmmm tudo certo pode dar npm start, escanear o código e se divirta com o bot :)"
+    echo $GREEN" ja PODE USAR MANOW"
 fi
 if [ "$inp" = "n" ]; then
-    echo $RED"Jae então, se divirta tentando descobrir as dependências sozinho e manualmente :)"
+    echo $RED"ok"
     exit
 fi
 
